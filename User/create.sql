b50fb8e1f3037ab440985c476b2c2744c5b0a171
Achsuthan <achsuthan@icloud.com>
2022-03-12 19:39:35 +0800

    🐛  user table details updated

--- User/create.sql
+++ User/create.sql
@@ -1,15 +1,14 @@
---- create User table script
 CREATE TABLE `user` (
-  `user_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL AUTO_INCREMENT,
   `email` varchar(64) NOT NULL,
   `name` varchar(64) DEFAULT NULL,
-  `salt` varchar(64) DEFAULT NULL,
-  `password` varchar(64) DEFAULT NULL,
+  `salt` varchar(64) NOT NULL,
+  `password` varchar(64) NOT NULL,
   `created_date` datetime DEFAULT NULL,
   `updated_date` datetime DEFAULT NULL,
-  `currency_id` int(11) NOT NULL AUTO_INCREMENT,
+  `currency_id` int(11) NOT NULL,
   PRIMARY KEY (`user_id`),
   UNIQUE KEY `email_UNIQUE` (`email`),
-  KEY `fk_currency_id_idx` (`currency_id`),
-  CONSTRAINT `fk_currency_id` FOREIGN KEY (`currency_id`) REFERENCES `currency` (`currency_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+  KEY `fk_user_currency_id_idx` (`currency_id`),
+  CONSTRAINT `fk_user_currency_id` FOREIGN KEY (`currency_id`) REFERENCES `currency` (`currency_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
 );
