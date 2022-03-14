2ea634b875bf106aee8b44ed30553b88429df151
yuyulwin <yuylwin5758@gmail.com>
2022-03-14 10:23:15 +0800

    Add files via upload

---
+++ Bank Transaction/insert.sql
@@ -0,0 +1,4 @@
+INSERT INTO `bank_transaction` values 
+(1, 'Food', 12.45, 'SGD', 'Transfer for food', '2022-03-13 00:00:01', '2022-03-13 00:00:01', 3, 1),
+(2, 'Household', 28.90, 'SGD', 'Buy tissue and handsoap', '2022-03-13 00:00:01', '2022-03-13 00:00:01', 2, 3),
+(3, 'Salary', 2000, 'SGD', 'Salary for March', '2022-03-13 00:00:01', '2022-03-13 00:00:01', 1, 1);
