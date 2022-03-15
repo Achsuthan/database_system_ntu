da1d73e207d52110eb6476e3f6f21ba226391a01
Yu Yu Lwin <yuylwin5758@gmail.com>
2022-03-15 22:42:28 +0800

    update data type

--- Invite/create.sql
+++ Invite/create.sql
@@ -1,12 +1,12 @@
 CREATE TABLE `invite`(
-	`invite_id` int(11) NOT NULL AUTO_INCREMENT,
+    `invite_id` int(11) NOT NULL AUTO_INCREMENT,
     `email` varchar(64) NOT NULL,
-    `expire_time` datetime DEFAULT NULL,
-    `created_date` datetime DEFAULT NULL,
-    `updated_date` datetime DEFAULT NULL,
-    `created_by` int(11) DEFAULT NULL,
+    `expire_time` datetime DEFAULT CURRENT_TIMESTAMP,
+    `created_date` datetime DEFAULT CURRENT_TIMESTAMP,
+    `updated_date` datetime DEFAULT CURRENT_TIMESTAMP,
+    `created_by` int(11) NOT NULL,
     PRIMARY KEY (`invite_id`),
     UNIQUE KEY `email_UNIQUE` (`email`),
     KEY `fk_invite_created_by_idx` (`created_by`),
-    CONSTRAINT `fk_invite_created_by` FOREIGN KEY (`created_by`) REFERENCES `user` (`user_id`) ON DELETE NO ACTION ON UPDATE NO ACTION    
+    CONSTRAINT `fk_invite_created_by` FOREIGN KEY (`created_by`) REFERENCES `user` (`user_id`) ON UPDATE CASCADE
 );
