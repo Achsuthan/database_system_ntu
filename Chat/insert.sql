16b9016efb64f23a2e410d95cc1f2ddd186ac0e6
Aung Kyaw Thant <akthant@gmail.com>
2022-03-15 14:36:36 +0800

    import data

---
+++ Chat/insert.sql
@@ -0,0 +1,4 @@
+INSERT INTO `chat` 
+VALUES (1,'Hi buddy, I sent you money. please check.','2022-03-15 06:17:18','2022-03-15 06:17:18',1,2),
+(2,'received with thanks!','2022-03-15 06:17:18','2022-03-15 06:17:18',1,1),
+(3,'I had transferred.','2022-03-15 06:17:19','2022-03-15 06:17:19',1,3);
