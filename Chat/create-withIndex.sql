377dbf6d024f05a77a466b44c8010cd398bfb397
Aung Kyaw Thant <akthant@gmail.com>
2022-03-19 07:58:33 +0800

    create indexes in tables

---
+++ Chat/create-withIndex.sql
@@ -0,0 +1,14 @@
+CREATE TABLE `chat` (
+  `chat_id` int(11) NOT NULL AUTO_INCREMENT,
+  `message` varchar(255) DEFAULT NULL,
+  `createddate` datetime DEFAULT CURRENT_TIMESTAMP,
+  `updateddate` datetime DEFAULT CURRENT_TIMESTAMP,
+  `transaction_id` int(11) NOT NULL,
+  `sender_id` int(11) NOT NULL,
+  PRIMARY KEY (`chat_id`),
+  KEY `fk_chat_transaction_id_idx` (`transaction_id`),
+  KEY `fk_chat_sender_id_idx` (`sender_id`),
+  KEY `idx_chat_message` (`message`),
+  CONSTRAINT `fk_chat_sender_id` FOREIGN KEY (`sender_id`) REFERENCES `user` (`user_id`) ON DELETE NO ACTION ON UPDATE CASCADE,
+  CONSTRAINT `fk_chat_transaction_id` FOREIGN KEY (`transaction_id`) REFERENCES `transaction` (`transaction_id`) ON UPDATE CASCADE
+);
