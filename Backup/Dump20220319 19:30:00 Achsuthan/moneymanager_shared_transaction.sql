909ce471ecffc9c0ebf41decc949a7328fa4f5d5
Achsuthan <achsuthan@icloud.com>
2022-03-19 19:34:00 +0800

    backup added

---
+++ Backup/Dump20220319 19:30:00 Achsuthan/moneymanager_shared_transaction.sql
@@ -0,0 +1,62 @@
+-- MySQL dump 10.13  Distrib 8.0.26, for macos11 (x86_64)
+--
+-- Host: ntudbms-1.mysql.database.azure.com    Database: moneymanager
+-- ------------------------------------------------------
+-- Server version	5.7.32-log
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `shared_transaction`
+--
+
+DROP TABLE IF EXISTS `shared_transaction`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `shared_transaction` (
+  `shared_transaction_id` int(11) NOT NULL AUTO_INCREMENT,
+  `percentage` double DEFAULT '0',
+  `createddate` datetime DEFAULT CURRENT_TIMESTAMP,
+  `updateddate` datetime DEFAULT CURRENT_TIMESTAMP,
+  `sender_id` int(11) DEFAULT NULL,
+  `receiver_id` int(11) DEFAULT NULL,
+  `transaction_id` int(11) NOT NULL,
+  PRIMARY KEY (`shared_transaction_id`),
+  KEY `fk_shared_transaction_sender_id_idx` (`sender_id`),
+  KEY `fk_shared_transaction_transaction_id_idx` (`transaction_id`),
+  KEY `fk_shared_transaction_receiver_id_idx` (`receiver_id`),
+  CONSTRAINT `fk_shared_transaction_receiver_id` FOREIGN KEY (`receiver_id`) REFERENCES `user` (`user_id`) ON DELETE NO ACTION ON UPDATE CASCADE,
+  CONSTRAINT `fk_shared_transaction_sender_id` FOREIGN KEY (`sender_id`) REFERENCES `user` (`user_id`) ON DELETE NO ACTION ON UPDATE CASCADE,
+  CONSTRAINT `fk_shared_transaction_transaction_id` FOREIGN KEY (`transaction_id`) REFERENCES `transaction` (`transaction_id`) ON DELETE NO ACTION ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `shared_transaction`
+--
+
+LOCK TABLES `shared_transaction` WRITE;
+/*!40000 ALTER TABLE `shared_transaction` DISABLE KEYS */;
+INSERT INTO `shared_transaction` VALUES (1,33.33,'2022-03-15 06:12:59','2022-03-15 06:12:59',2,1,1),(2,33.33,'2022-03-15 06:13:33','2022-03-15 06:13:33',3,1,1),(3,33.33,'2022-03-15 13:14:32','2022-03-15 13:14:32',2,2,1),(6,33.33,'2022-03-19 11:01:41','2022-03-19 11:01:41',3,2,10),(7,33.33,'2022-03-19 11:01:41','2022-03-19 11:01:41',1,2,10),(8,33.33,'2022-03-19 11:01:41','2022-03-19 11:01:41',2,2,10);
+/*!40000 ALTER TABLE `shared_transaction` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-03-19 19:27:53
---
+++ Backup/Dump20220319 19:30:00 Achsuthan/moneymanager_user.sql
@@ -0,0 +1,62 @@
+-- MySQL dump 10.13  Distrib 8.0.26, for macos11 (x86_64)
+--
+-- Host: ntudbms-1.mysql.database.azure.com    Database: moneymanager
+-- ------------------------------------------------------
+-- Server version	5.7.32-log
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `user` (
+  `user_id` int(11) NOT NULL AUTO_INCREMENT,
+  `email` varchar(64) NOT NULL,
+  `name` varchar(64) DEFAULT NULL,
+  `salt` varchar(64) NOT NULL,
+  `password` varchar(64) NOT NULL,
+  `created_date` datetime DEFAULT CURRENT_TIMESTAMP,
+  `updated_date` datetime DEFAULT CURRENT_TIMESTAMP,
+  `currency_id` int(11) NOT NULL,
+  PRIMARY KEY (`user_id`),
+  UNIQUE KEY `email_UNIQUE` (`email`),
+  UNIQUE KEY `idx_user_email` (`email`),
+  KEY `fk_user_currency_id_idx` (`currency_id`),
+  KEY `idx_user_name` (`name`),
+  CONSTRAINT `fk_user_currency_id` FOREIGN KEY (`currency_id`) REFERENCES `currency` (`currency_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user`
+--
+
+LOCK TABLES `user` WRITE;
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+INSERT INTO `user` VALUES (1,'achsuthan@gmail.com','Achsuthan','some salt','some password','2022-03-12 00:00:01','2022-03-12 00:00:01',1),(2,'aung@gmail.com','Aung Kyaw Thant','some salt for aung','some password for aung','2022-03-12 00:00:01','2022-03-12 00:00:01',1),(3,'yuyu@gmail.com','Yu Yu Lwin','some salt for Yu Yu','some password for yu yu','2022-03-12 00:00:01','2022-03-12 00:00:01',1),(4,'senthuran@gmail.com','Senthuran','some salt for senthuran','some password for senthuran','2022-03-19 00:00:01','2022-03-19 00:00:01',1),(5,'jhon@gmail.com','Jhon','some salt Jhon','Some password for Jhon','2022-03-19 00:00:01','2022-03-19 00:00:01',1),(6,'ram@gmail.com','Ram','some salt for Ram','some password for ram','2022-03-14 00:00:01','2022-03-13 00:00:01',1),(7,'peter@gmail.com','Peter','some salt for Peter','some password for peter','2022-03-10 00:00:01','2022-03-10 00:00:01',1),(8,'hung@gmail.com','Hung','some salt for Hung','some password for Hung','2022-03-06 00:00:01','2022-03-06 00:00:01',1),(9,'suhas@gmail.com','Suhas','some salt for Suhas','some password for Suhas','2022-03-08 00:00:01','2022-03-09 00:00:01',1),(10,'thabib@gmail.com','Thabib','some salt for thabib','some password for thabib','2022-03-10 00:00:01','2022-03-10 00:00:01',1),(11,'thony@gmail.com','Tony','some salt for tony','some password for tony','2022-03-16 00:00:01','2022-03-16 00:00:01',1),(12,'jenson@gmail.com','JEnson','some salt for jenson','some password for jenson','2022-03-16 00:00:01','2022-03-16 00:00:01',1),(13,'boon@gmail.com','Boon','some salt for Boon','some password for bpon','2022-03-16 00:00:01','2022-03-16 00:00:01',1),(14,'stanly@gmail.com','Stanly','some salt for stanly','some password for stanly','2022-03-18 00:00:01','2022-03-18 00:00:01',1),(15,'karthik@gmail.com','Karthik','some salt for karthik','some password for karthik','2022-03-21 00:00:01','2022-03-21 00:00:01',1);
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-03-19 19:28:36
