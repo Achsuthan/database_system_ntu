123b6bc8b694ea0d8feb68a5b43bcad847588e0b
Achsuthan <achsuthan@icloud.com>
2022-03-19 14:06:49 +0800

    📝 pagination comments added

--- Scripts/pagination.sql
+++ Scripts/pagination.sql
@@ -1,5 +1,18 @@
---- Need to work on this
 
+/*
+    Intro: This SQL query is used to do the pagination for all the queries
+    Assumption: 
+        1. For this query used only for the user search pagination, but same implementation can be added to all other Queries if requires
+        2. Backend langugue should pass the how many page they wanted for specific page (value for limit) and the current page number(offset)
+    DataRepresentation: The backend script will provide the table with user details, 
+        User Id
+        User name
+        User email
+    
+    These table details will be represented as JSON/XML or HTML table format using ORM and send it back to frontend
+*/
+
+--- Need to work on this for the user table search query
 
 SELECT * 
 FROM transaction
