d79859bf53965113c6b1a20dc1aed67fcfece73f
Aung Kyaw Thant <akthant@gmail.com>
2022-04-01 21:59:48 +0800

    chat table with transaction (join not required)

--- Scripts/Aung/6. ChatDetailsByTransactionID.sql
+++ Scripts/Aung/6. ChatDetailsByTransactionID.sql
@@ -2,39 +2,18 @@
     Intro: This SQL query is used to get chat messages for group transaction by acending order of message sent.
     Assumption: Frontend will have the seperate page to get all the messages for each group transaction and display it to user
     DataRepresentation: The backend script will provide the table with transaction details, 
-        transaction Id
-        transaction name 
-        transaction amount
-		Group transaction Id
-        Group transaction name 
-        transaction type (Expenses/Income)
+        chat Id
         sender name
-        sender email
+        smessage
+        sent out date time
     <> These table details will be represented as JSON/XML or HTML table format using ORM and send it back to frontend
 */
 
 
 
-SET @groupTransactionId = 43; 
-SET @transactionSpentFor = 3; -- group transaction
-SET @transactionTypeId = 3; -- transfer type
+SET @TransactionId = 43; 
 
-SELECT transaction.transaction_id as transaction_id, transaction.name as transaction_name, transaction.amount as total_transaction_amount,
-transaction.description as transaction_description,
-transaction_type.type as transactiontype,
-spending_group.name as spendinggroupname,
-getUserNameById(chat.sender_id) as sender_name,
-getUserEmailById(chat.sender_id) as sender_email,
-chat.message,
-chat.createddate as sent_time
-FROM transaction
-inner join (SELECT DISTINCT transaction_id, group_id FROM group_transaction) AS T on transaction.transaction_id = T.transaction_id
-inner join spending_group on T.group_id = spending_group.spending_group_id
-inner join category on (transaction.category_id = category.category_id)
-inner join transaction_type on (transaction.transaction_type_id = transaction_type.transaction_type_id)
-inner join chat on transaction.transaction_id = chat.transaction_id
-inner join user on chat.sender_id = user.user_id
-where 	transaction.transaction_spent_for_id = @transactionSpentFor &&  -- Spend for Group only
-        transaction_type.transaction_type_id = @transactionTypeId && -- transfer type
-        transaction.transaction_id = @groupTransactionId
+SELECT chat.chat_id, getUserNameById(chat.sender_id) as sender_name, chat.message, chat.createddate as sent_time
+FROM chat
+where 	chat.transaction_id = @TransactionId
 order by chat.createddate asc;
