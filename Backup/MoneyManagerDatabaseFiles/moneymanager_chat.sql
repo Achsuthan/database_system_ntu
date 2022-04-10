490051d3773a2c0a4cda12ac33c7e96f5f9b31a1
Achsuthan <achsuthan@icloud.com>
2022-04-10 20:25:08 +0800

    MoneyManager database files added basedon the report

---
+++ Backup/MoneyManagerDatabaseFiles/moneymanager_chat.sql
@@ -0,0 +1,60 @@
+-- MySQL dump 10.13  Distrib 8.0.26, for macos11 (x86_64)
+--
+-- Host: ntudbms-1.mysql.database.azure.com    Database: moneymanager
+-- ------------------------------------------------------
+-- Server version	5.7.37-log
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `chat`
+--
+
+DROP TABLE IF EXISTS `chat`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `chat` (
+  `chat_id` int(11) NOT NULL AUTO_INCREMENT,
+  `message` varchar(255) DEFAULT NULL,
+  `createddate` datetime DEFAULT CURRENT_TIMESTAMP,
+  `updateddate` datetime DEFAULT CURRENT_TIMESTAMP,
+  `transaction_id` int(11) NOT NULL,
+  `sender_id` int(11) NOT NULL,
+  PRIMARY KEY (`chat_id`),
+  KEY `fk_chat_transaction_id_idx` (`transaction_id`),
+  KEY `fk_chat_sender_id_idx` (`sender_id`),
+  KEY `idx_chat_message` (`message`),
+  CONSTRAINT `fk_chat_sender_id` FOREIGN KEY (`sender_id`) REFERENCES `user` (`user_id`) ON DELETE NO ACTION ON UPDATE CASCADE,
+  CONSTRAINT `fk_chat_transaction_id` FOREIGN KEY (`transaction_id`) REFERENCES `transaction` (`transaction_id`) ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `chat`
+--
+
+LOCK TABLES `chat` WRITE;
+/*!40000 ALTER TABLE `chat` DISABLE KEYS */;
+INSERT INTO `chat` VALUES (1,'Hi buddy, I sent you money. please check.','2022-03-15 06:17:18','2022-03-15 06:17:18',43,4),(2,'received with thanks!','2022-03-15 06:17:18','2022-03-15 06:17:18',43,7),(3,'I had transferred.','2022-03-15 06:17:19','2022-03-15 06:17:19',43,6),(4,'receive. many thanks!','2022-03-15 05:17:19','2022-03-15 05:17:19',43,7);
+/*!40000 ALTER TABLE `chat` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-04-06  8:02:53
